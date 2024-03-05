2dd364effe5b74bde9ed68401198e659979bb095
Shreya Arora <72164032+shreyarora12@users.noreply.github.com>
2024-03-06 02:23:47 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{Use openweathermap.org for your API key generation}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
